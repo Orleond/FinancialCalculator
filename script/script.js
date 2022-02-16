2323b3686133b40b9b0b620fd446602a8b3c0466
Mr. Orleond <mr.orleond@gmail.com>
2022-02-16 11:12:14 +0300

    Присвоение некоторым переменным значений, вывод в консоль

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
 'use strict';
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 1000,                                   // Доход за месяц
+    income = "Фриланс",                             // Дополнительный доход
+    addExpenses = "Цветы, коммуналка, аренда",      // Дополнительные расходы
+    deposit = false,                                // Депозит в банке
+    mission = 2000,                                 // Желаемая сумма накопления
+    period = 12,                                    // Период накопления
+    budgetDay = money / 30;                         // Дневной бюджет
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(", ");
+console.log(addExpenses);
+
+// Вывод дневного бюджета в консоль
+console.log("Дневной бюджет равен " + budgetDay + " рублей");
 
 alert('Привет!');
 console.log('Привет!');
