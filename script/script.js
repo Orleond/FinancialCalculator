e6a98a67abcc19a02dec4a57410d42858fff5a9f
Mr. Orleond <mr.orleond@gmail.com>
2022-02-16 14:06:15 +0300

    Написание функций для манипуляций с финансами

--- script/script.js
+++ script/script.js
@@ -2,33 +2,21 @@
 
 let money,                                          // Доход за месяц
     income = "Фриланс",                             // Дополнительный доход
-    addExpenses = "Цветы, коммуналка, аренда",      // Дополнительные расходы
-    deposit = false,                                // Депозит в банке
+    addExpenses,                                    // Дополнительные расходы
+    deposit,                                        // Депозит в банке
     mission = 2000,                                 // Желаемая сумма накопления
     period = 12,                                    // Период накопления
     budgetDay,                                      // Дневной бюджет
-    budgetMonth,                                    // Месячный бюджет
     expenses1,                                      // Обязательная статья расхода 1
     expenses2,                                      // Обязательная статья расхода 2
     amount1,                                        // Размер обязательного расхода 1
-    amount2;                                        // Размер обязательного расхода 2
+    amount2,                                        // Размер обязательного расхода 2
+    accumulatedMonth;                               // Накопления за месяц
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log(addExpenses.length);
-
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей");
-
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(", ");
-console.log(addExpenses);
-
-
-alert('Привет!');
-console.log('Привет!');
+// Определение типов данных переменных
+let showTypeOf = function(data) {
+    console.log(data, typeof data);
+};
 
 // Ввод данных
 money = prompt("Ваш месячный доход?", 1000);
@@ -39,23 +27,50 @@ amount1 = prompt("Во сколько это обойдется?", 100);
 expenses2 = prompt("Введите обязательную статью расходов?", "Квартплата");
 amount2 = prompt("Во сколько это обойдется?", 130);
 
-// Вычисление месячного бюджета
-budgetMonth = money - amount1 - amount2;
+
+// Сумма всех обязательных расходов
+let getExpensesMonth = function(amount1, amount2) {
+    return amount1 + amount2;
+};
+
+// Накопления за месяц
+let getAccumulatedMonth = function(money, amount1, amount2) {
+    return money - amount1 - amount2;
+};
+accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
+
+// Вычисление периода достижения цели
+let getTargetMonth = function(mission, accumulatedMonth){
+    return Math.ceil(mission / accumulatedMonth);
+};
+
+// Определение уровня доходов
+let getStatusIncome = function(budgetDay) {
+    if (budgetDay >= 100) {
+        return "У вас высокий уровень дохода";
+    } else if (budgetDay >= 50) {
+        return "У вас средний уровень дохода";
+    } else if (budgetDay >= 0) {
+        return "К сожалению, у вас уровень дохода ниже среднего";
+    } else {
+        return "Что-то пошло не так";
+    }
+};
 
 // Вычисление дневного бюджета
-budgetDay = budgetMonth / 30;
+budgetDay = Math.floor(accumulatedMonth / 30);
+
+// Разбиение дополнительных расходов на массив
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(", ");
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
 // Вывод в консоль
+console.log("Расходы за месяц составляют " + getExpensesMonth(amount1, amount2) + " рублей");
+console.log("Возможные расходы - " + addExpenses);
+console.log("Цель будет достигнута за " + getTargetMonth(mission, accumulatedMonth) + " месяца(ев)");
 console.log("Дневной бюджет равен " + budgetDay + " рублей");
-console.log("Месячный бюджет равен " + budgetMonth + " рублей");
-console.log("Цель будет достигнута за " + Math.ceil(mission / budgetMonth) + " месяца(ев)");
-
-if (budgetDay >= 100) {
-    console.log("У вас высокий уровень дохода");
-} else if (budgetDay >= 50) {
-    console.log("У вас средний уровень дохода");
-} else if (budgetDay >= 0) {
-    console.log("К сожалению, у вас уровень дохода ниже среднего");
-} else {
-    console.log("Что-то пошло не так");
-}
+console.log(getStatusIncome(budgetDay));
